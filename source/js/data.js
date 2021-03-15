89c6fa4c52f61f23e7f71d15143b516af71fa403
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-03-15 22:57:25 +0300

    Добавляет данные

--- source/js/data.js
+++ source/js/data.js
@@ -26,6 +26,35 @@ const NEWS_LIST = [{
     'link': 'https://htmlacademy.ru/blog/boost/tools/time-trackers',
     'status': 0,
   },
+  {
+    'title': 'Как заработать на frontend 100 миллионов за 30 минут',
+    'author': 'Георгий Разведяйкин',
+    'data': '2022-05-3 21:15',
+    'link': 'https://htmlacademy.ru/blog/boost/tools/time-trackers',
+    'status': 0,
+  },
+  {
+    'title': 'Новый тренд - котики в тапочках за компьютером',
+    'author': 'Марина Романова',
+    'data': '2010-02-10 14:09',
+    'link': 'https://htmlacademy.ru/blog/boost/tools/time-trackers',
+    'status': 0,
+  },
+  {
+    'title': 'Новое расследование - куда исчезают престарелые программисты',
+    'author': 'Елизавета Хвостонога',
+    'data': '2011-02-23 15:00',
+    'link': 'https://htmlacademy.ru/blog/boost/tools/time-trackers',
+    'status': 0,
+  },
+
+  {
+    'title': 'Лучшая книга по программированию по мнению критиков',
+    'author': 'Света Могучая',
+    'data': '2011-03-03 10:00',
+    'link': 'https://htmlacademy.ru/blog/boost/tools/time-trackers',
+    'status': 0,
+  },
 ];
 
-export { NEWS_LIST}
+export {NEWS_LIST};
